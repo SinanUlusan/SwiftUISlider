ffc9d3dd6256f65b3e85222bd5e797598694dac7
SinanStatistician <SinanStatistician@users.noreply.github.com>
2020-12-29 01:55:10 +0300

    Initial Commit

---
+++ SwiftUISlider/SwiftUISliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISliderApp.swift
+//  SwiftUISlider
+//
+//  Created by Sinan Ulusan on 29.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
